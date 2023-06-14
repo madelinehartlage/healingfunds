91b3d86a969fcf632eebefff57859e42de2a4d7c
mhartlage3 <madeline.hartlage@gatech.edu>
2023-06-14 17:07:32 -0500

    draft donate functionality on main page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Text, Flex, Stack, Image, Button } from '@chakra-ui/react';
 import './App.css';
+import logoPNG from "./logoPNGAlt4.png";
+import seniorlove2 from "./image-1000x500.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex height="100vh">
+      <Stack direction="column" width="100%">
+        <Flex width="100%" paddingLeft={4}>
+          <Flex width="60%">
+            <Image src={logoPNG}/>
+          </Flex>
+          <Stack paddingTop={12} direction="row" width="40%" spacing={10} alignItems="center">
+            <Text fontSize="xl" fontWeight="bold">
+              ABOUT
+            </Text>
+            <Text fontSize="xl" fontWeight="bold">
+              SPONSORS
+            </Text>
+            <Text  fontSize="xl" fontWeight="bold">
+              ARTICLES
+            </Text>
+            <Text fontSize="xl" fontWeight="bold">
+              CONTACT
+            </Text>
+            <Text fontSize="xl" fontWeight="bold">
+              DONATE
+            </Text>
+          </Stack>
+        </Flex>
+        <Image src={seniorlove2}/>
+        <Flex width="100%" justifyContent="center">
+          <Stack direction="column" width="50%">
+            <Text fontWeight="bold" fontSize="2xl" textAlign="center">
+              Healing Cancer Holistically
+            </Text>
+            <Text textAlign="center" fontWeight="semibold">
+              The mission of Healing Funds is to distribute funds to cancer patients for the purpose of utilizing holistic alternative methods for treatment.
+            </Text>
+            <Text textAlign="center" fontWeight="semibold">
+              Healing Funds seeks to provide cancer patients with the financial resources necessary to approach healing naturally. 
+              Whether it be exercise, diet, therapy, or other methods, each patient deserves the ability to fight cancer in a way that protects 
+              and enhances their health.
+            </Text>
+            <Text textAlign="center" fontWeight="semibold">
+              With your donation, Healing Funds can enable a patient in need to receive revitalizing care.
+            </Text>
+            <Flex width="100%" justifyContent="space-around" paddingTop={8}>
+              <Button bgColor="#439298" color="white" borderRadius="0%" paddingRight={6} paddingLeft={6} paddingTop={4} paddingBottom={4}>
+                DONATE NOW
+              </Button>
+              <Button bgColor="#F86F8B" color="white" borderRadius="0%" paddingRight={6} paddingLeft={6} paddingTop={4} paddingBottom={4}>
+                REQUEST FUNDS
+              </Button>
+            </Flex>
+          </Stack>
+        </Flex>
+      </Stack>
+    </Flex>
   );
 }
 
