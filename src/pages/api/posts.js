4e1997e80bf2a9890da156e6c7e102f4ec6043c3
mhartlage3 <madeline.hartlage@gatech.edu>
2023-07-12 19:20:09 -0500

    database fixes

--- src/pages/api/posts.js
+++ src/pages/api/posts.js
@@ -3,15 +3,19 @@ import clientPromise from "../../db/mongodb";
 export default async function handler(req, res) {
   const client = await clientPromise;
   const db = client.db("nextjs-mongodb-demo");
-  switch (req.method) {
-    case "POST":
-      let bodyObject = JSON.parse(req.body);
-      let myPost = await db.collection("posts").insertOne(bodyObject);
-      res.json(myPost);
-      break;
-    case "GET":
-      const allPosts = await db.collection("posts").find({}).toArray();
-      res.json({ status: 200, data: allPosts });
-      break;
+  try {
+    switch (req.method) {
+      case "POST":
+        let bodyObject = JSON.parse(req.body);
+        let myPost = await db.collection("posts").insertOne(bodyObject);
+        res.json(myPost);
+        break;
+      case "GET":
+        const allPosts = await db.collection("posts").find({}).toArray();
+        res.json({ status: 200, data: allPosts });
+        break;
+    }
+  } catch(error) {
+    return {status: 500, body: error.toString()}
   }
 }
