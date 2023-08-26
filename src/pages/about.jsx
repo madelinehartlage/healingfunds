84991b10eb8217b178296fc49597948798dd5979
mhartlage3 <madeline.hartlage@gatech.edu>
2023-08-25 20:12:58 -0400

    visual bug fix

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -390,7 +390,7 @@ function About() {
         </Flex>
         </Stack>
         
-        <Stack bgColor="#439298" width="100%" direction="row" justifyContent={["space-around","center"]} spacing={[0, 70]} marginTop={[40, 5]} paddingTop={10} paddingBottom={10} position={[(user && user.name && user.name == "adminpasscode") ? "relative" : "absolute", "relative"]} bottom={0}>
+        <Stack bgColor="#439298" width="100%" direction="row" justifyContent={["space-around","center"]} spacing={[0, 70]} marginTop={[40, 5]} paddingTop={10} paddingBottom={10} position={["relative", (user && user.name && user.name == "adminpasscode") ? "relative" : "absolute"]} bottom={0}>
           <Link href="/"> 
             <Text fontSize="lg" fontWeight="semibold" color="white">
               Healing Funds Inc.
